db9d3e28330604a1e9f9dd7e1c695f1d361da1b0
maowarisa <maowarisa123@gmail.com>
2024-04-24 13:34:56 +1000

    load data on route using loader and useLoaderData

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,8 +1,22 @@
-
+import { useLoaderData } from "react-router-dom";
+import User from "../User/User";
+import './Users.css'
 const Users = () => {
+    // state --> data
+    // state --> loader
+    // use effect 
+    // fetch --> state set --> set state
+
+    const users = useLoaderData();
     return (
         <div>
-            <h2>Our Users</h2>
+            <h2>Our Users: {users.length}</h2>
+            <p>They all our respected users</p>
+            <div className="users">
+                {
+                    users.map((user) => <User key={user.id} user={user}></User>)
+                }
+            </div>
         </div>
     );
 };
